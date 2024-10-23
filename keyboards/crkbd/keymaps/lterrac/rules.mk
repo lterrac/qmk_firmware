130ce8d295b2b36f28dd4a0099020918ee43e964
Luca Terracciano <terraccianoluca@gmail.com>
2024-10-23 22:49:28 +0200

    add lterrac layout

---
+++ keyboards/crkbd/keymaps/lterrac/rules.mk
@@ -0,0 +1,10 @@
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = yes
+OLED_DRIVER_ENABLE = yes
+
+# https://github.com/qmk/qmk_firmware/blob/master/docs/config_options.md#build-options
+# significantly reduce the compiled size
+LTO_ENABLE = yes
+
+SLEEP_LED_ENABLE = no
+
